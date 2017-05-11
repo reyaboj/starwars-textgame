5ea0e769ae4a3e6cfa3b5bb8efc5272d12e1a535
Md Istiaque Al Jobayer <mial2@student.monash.edu>
2017-05-12 00:17:31 +0800

    Add comments to C3P0 class and random monologues for C3P0

--- FIT2099_Assignment/src/starwars/entities/actors/C3P0.java
+++ FIT2099_Assignment/src/starwars/entities/actors/C3P0.java
@@ -9,6 +9,8 @@ import starwars.Team;
  * C3P0 Droid -- Human/Cyborg Relations
  */
 public class C3P0 extends Droid {
+
+    /** Monologue pool to select from */
     private String[] monologue = {
             "Artoo says that the chances of survival are 725 to 1. Oh dear...",
             "We're doomed.",
@@ -20,21 +22,43 @@ public class C3P0 extends Droid {
             "Oh dear, I might be lost."
     };
 
+    /**
+     * Initialize C3P0 droid.
+     *
+     * @param owner the owner
+     * @param team the team it should belong to
+     * @param hitpoints the initial hitpoints
+     * @param m message renderer for displaying on the view
+     * @param world the containing world
+     */
     public C3P0(SWActor owner, Team team, int hitpoints, MessageRenderer m, SWWorld world) {
         super(owner, team, hitpoints, m, world);
     }
 
+    /**
+     * C3P0's default behavior. C3P0 has a 10% chance of saying something.
+     */
     @Override
     public void actDefault() {
+        if (Math.random() >= 0.1)  // 10% chance of saying something
+            return;
         String randomMonologue = monologue[(int)(Math.random() * monologue.length)];
         say(getShortDescription() + " says: " + randomMonologue);
     }
 
+    /**
+     * C3P0's short description.
+     * @return the description
+     */
     @Override
     public String getShortDescription() {
         return "C3P0";
     }
 
+    /**
+     * C3P0's long description.
+     * @return the description
+     */
     @Override
     public String getLongDescription() {
         return "C3P0, Human-Cyborg Relations";
